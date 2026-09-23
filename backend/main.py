b0355b68bac08dfcd7d44c2b2b46c8687894ab0b
Tojt1 <konsikwojciech@onet.pl>
2026-09-23 17:23:02 +0200

    first sql table

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from database import engine
+from sqlalchemy import text
 app = FastAPI()
 
 @app.get("/")
 def say_hello():
-    return {"information": "Hello world!"}
+    return {"information": "Hello World!"}
+
+@app.get("/db-health")
+def check_db():
+    with engine.connect() as con:
+        con.execute(text("SELECT 1"))
+
+    return {"db":"connected"}
